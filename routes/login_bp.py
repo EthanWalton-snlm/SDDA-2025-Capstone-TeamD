6fce723acafc733aba3c7884dd622d55d2c91aec
Chleo-Smith <Chleo.Smith@sanlam.co.za>
2025-03-28 20:16:35 +0200

    navigation bar to login finalized

--- routes/login_bp.py
+++ routes/login_bp.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 login_bp = Blueprint("login_bp", __name__)
 
 
-@login_bp.get("/login")
+@login_bp.get("/log-in")
 def log_in_screen():
     return render_template("login.html")
