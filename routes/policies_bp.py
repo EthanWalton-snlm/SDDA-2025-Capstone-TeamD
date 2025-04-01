6450530c2f85b2179a6782047639e9f76c94950e
Chleo-Smith <Chleo.Smith@sanlam.co.za>
2025-04-01 18:46:46 +0200

    policy added to database on click

--- routes/policies_bp.py
+++ routes/policies_bp.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
+
+from constants import set_logged_in_username
+from extensions import db
+from models.policies import Policies
+from utilities import encrypt_password
 
 policies_bp = Blueprint("policies_bp", __name__)
 
@@ -11,3 +16,24 @@ def policies_screen():
 @policies_bp.get("/policies/new-policy")
 def new_policy_page():
     return render_template("new-policy.html")
+
+
+@policies_bp.post("/policies/new-policy-sign-up")
+def new_policy_sign_up():
+    data = {
+        "username": request.form.get("username"),
+        "phone_name": request.form.get("phone_name"),
+        "policy_name": request.form.get("radio"),
+    }
+
+    new_user = Policies(**data)
+    print(new_user.to_dict())
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        set_logged_in_username(new_user.to_dict()["username"])
+        return redirect(url_for("dashboard_bp.dashboard_page"))
+    except Exception as e:
+        db.session.rollback()  # restores data, cannot be done after commit()
+        print(e)
+        return redirect(url_for("home_bp.home_screen"))
