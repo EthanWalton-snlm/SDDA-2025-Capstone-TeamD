df3fd90dadf4033c862eb9feafd70d7e7ae07073
Chleo-Smith <Chleo.Smith@sanlam.co.za>
2025-04-02 17:48:24 +0200

    adding unstaged files

--- routes/dashboard_bp.py
+++ routes/dashboard_bp.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 
-from constants import get_logged_in_username
 from models.users import User
 
 dashboard_bp = Blueprint("dashboard_bp", __name__)
@@ -10,6 +9,6 @@ dashboard_bp = Blueprint("dashboard_bp", __name__)
 @dashboard_bp.get("/")
 @login_required
 def dashboard_page():
-    print("welcome " + get_logged_in_username())
+    print("welcome ")
     # print(get_policies())
     return render_template("dashboard.html")
---
+++ routes/login_bp.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, redirect, render_template, request, url_for
+
+from routes.account_details_bp import get_user_details
+
+login_bp = Blueprint("login_bp", __name__)
+
+
+@login_bp.get("/log-in")
+def log_in_screen():
+    return render_template("login.html")
+
+
+@login_bp.post("/log-in-auth")  # TODO: send details in JSON body
+def log_in_auth():
+    data = {
+        "username": request.form.get("username"),
+        "password": encrypt_password(
+            request.form.get("password")
+        ),  # TODO: encrypt in html [using encrypt_password()]
+    }
+
+    user = get_user_details(data["username"])
+
+    if user is None:
+        return redirect(url_for("login_bp.log_in_screen"))
+
+    print(user.to_dict()["password"], data["password"])
+
+    if user.to_dict()["password"] == data["password"]:
+        set_logged_in_username(user.to_dict()["username"])
+        return redirect(url_for("dashboard_bp.dashboard_page"))
+    else:
+        return redirect(url_for("login_bp.log_in_screen"))
