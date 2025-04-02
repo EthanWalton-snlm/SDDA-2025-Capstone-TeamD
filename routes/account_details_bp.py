1ebf6e77510b85e18576d2e44bcf53114d244090
Chleo-Smith <Chleo.Smith@sanlam.co.za>
2025-04-02 20:57:27 +0200

    populate user policies on dashboard

--- routes/account_details_bp.py
+++ routes/account_details_bp.py
@@ -2,12 +2,13 @@ from flask import Blueprint, redirect, render_template, request, url_for
 from flask_login import current_user
 
 from extensions import db
+from models.policies import Policies
 from models.users import User
 
 account_details_bp = Blueprint("account_details_bp", __name__)
 
 
-def get_user_details(username):
+def get_user_details(username, policy_id):
     return User.query.get(username)
 
 
