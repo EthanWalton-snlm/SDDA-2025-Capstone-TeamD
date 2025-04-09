8b67716bb2f15604d90216d080c197b0f3f115c3
Chleo-Smith <Chleo.Smith@sanlam.co.za>
2025-04-09 17:39:58 +0200

    claim counter increments

--- SEED_DATA.sql
+++ SEED_DATA.sql
@@ -12,9 +12,15 @@ CREATE TABLE users (
     email NVARCHAR(50),
     phone_number NVARCHAR(13),
     id_number NVARCHAR(13),
-	is_admin INT DEFAULT 0, -- 1 = True
-	profile_pic NVARCHAR(500) NULL
+    is_admin INT DEFAULT 0, -- 1 = True
+    profile_pic NVARCHAR(500),
+
+	claims_made INT DEFAULT 0,
+	claims_approved INT DEFAULT 0,
+	claims_rejected INT DEFAULT 0,
+	claims_pending INT DEFAULT 0
 );
+
 
 CREATE TABLE usersToContact (
     id_number NVARCHAR(13) PRIMARY KEY,
@@ -38,9 +44,9 @@ CREATE TABLE policies (
     premium FLOAT,
     phone_name NVARCHAR(50),
     policy_name NVARCHAR(50),
-    phone_case NVARCHAR(50),
-    screen_protector NVARCHAR(50),
-    waterproof_phone NVARCHAR(50),
+    phone_case INT,
+    screen_protector INT,
+    waterproof_phone INT,
     username NVARCHAR(100) REFERENCES users(username),
     policy_type_id NVARCHAR(50) REFERENCES policyTypes(policy_type_id),
     image_link NVARCHAR(500) NULL
@@ -51,9 +57,9 @@ CREATE TABLE claims (
     status NVARCHAR(25),
     reason NVARCHAR(500),
     admin_comment NVARCHAR(500),
-    affidavit_link NVARCHAR(MAX),
-    image_link NVARCHAR(MAX),
-    quotation_link NVARCHAR(MAX),
+    affidavit_link NVARCHAR(500),
+    image_link NVARCHAR(500),
+    quotation_link NVARCHAR(500),
     approved_by NVARCHAR(100),  -- column to store the admin who approved the claim
     rejected_by NVARCHAR(100),  -- column to store the admin who rejected the claim
     submission_date DATETIME NOT NULL DEFAULT GETDATE(), -- date when claim is submitted
@@ -64,13 +70,13 @@ CREATE TABLE claims (
 	amount_approved FLOAT
 );
 
-INSERT INTO users (username, password, first_name, last_name, is_admin, profile_pic)
-VALUES
-('john545', 'scrypt:32768:8:1$XX4iX48LUzwzHRST$b51014d60219704eccda5766b47dc760430333aa17e8848da5650f52e4427b71c6bbf2cb4ba0a6cd931c64a8173d9a5f6dfc0ab49f9d20820876009a17f97dd1', 'John', 'Steven', 0, 'static/Image/user_uploads/policy-2025-04-09-john545.png'); -- password: admin
+-- Add constraint to set default submission_date to current date/time when claim is created
+-- ALTER TABLE claims
+-- ADD CONSTRAINT DF_Claims_SubmissionDate DEFAULT (GETDATE()) FOR submission_date;
 
 INSERT INTO users (username, password, first_name, last_name, is_admin)
 VALUES
-('admin', 'scrypt:32768:8:1$XX4iX48LUzwzHRST$b51014d60219704eccda5766b47dc760430333aa17e8848da5650f52e4427b71c6bbf2cb4ba0a6cd931c64a8173d9a5f6dfc0ab49f9d20820876009a17f97dd1', 'Admin', 'Control', 1); -- password: admin
+('admin', 'scrypt:32768:8:1$4Ce2JLFzCwuMqtsi$a804f8cda8d67c01dfabd8837936899f3bf80c433577b922dbb9f27997683d1b76d7698e992827118d5bea047495c5e249a39c1d894c266d6bfd133319c25eb1', 'Admin', 'Account', 1);
 
 INSERT INTO policyTypes VALUES ('aFRESH', 'Fresher Plan', 'Covers Theft Protection', 50);
 INSERT INTO policyTypes VALUES ('bPERS', 'Personal Plan', 'Covers Theft Protection and Screen Damage Protection', 200);
@@ -78,18 +84,18 @@ INSERT INTO policyTypes VALUES ('cBUS', 'Business Plan', 'Covers Theft Protectio
 INSERT INTO policyTypes VALUES ('dPLAT', 'Platinum Plan', 'Covers Theft Protection, Screen Damage Protection, Water Damge Protection and Phone Upgrade Costs', 1000);
 
 
-INSERT INTO policies VALUES ('e6b4e44d-6838-4f44-bab8-03f2a075709b', 100, 'iPhone 13', 'Business Plan', 'Yes', 'Yes', 'No',
-    (SELECT username FROM users WHERE username = 'john545'),
-    (SELECT policy_type_id FROM policyTypes WHERE policy_type_id='cBUS'), 'static/Image/user_uploads/policy-2025-04-09-john545.png');
+INSERT INTO policies VALUES ('123', 100, 'iPhone 13', 'Business Plan', 'Yes', 'Yes', 'No',
+    (SELECT username FROM users WHERE username = 'admin'),
+    (SELECT policy_type_id FROM policyTypes WHERE policy_type_id='BPLAN'), 'test.jpg');
 
-
-INSERT INTO claims (claim_id, status, reason, admin_comment, date_of_incident, username, policy_id, claim_amount, amount_approved)
-VALUES ('e50ce2f4-a338-43c9-b09c-84525bc16929', 'Pending', 'broke my phone', '', '2025-03-15',
-    (SELECT username FROM users WHERE username = 'e6b4e44d-6838-4f44-bab8-03f2a075709b'),
+INSERT INTO claims (claim_id, status, reason, admin_comment, date_of_incident, username, policy_id)
+VALUES ('claim1', 'Pending', 'broke my phone', '', '2025-03-15',
+    (SELECT username FROM users WHERE username = 'admin'),
     (SELECT policy_id FROM policies WHERE policy_id='123'), 10000, 6000);
 
 SELECT * FROM users;
-SELECT * FROM policies;
 SELECT * FROM usersToContact;
 SELECT * FROM claims;
 SELECT * FROM policyTypes;
+
+SELECT * FROM policies;
