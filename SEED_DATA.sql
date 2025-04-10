3c4d858be3e9d65506a78420156e9666bf309766
EthanWalton-snlm <ethan.walton@sanlam.co.za>
2025-04-10 15:32:06 +0200

    seed data

--- SEED_DATA.sql
+++ SEED_DATA.sql
@@ -1,8 +1,13 @@
+-- Drop existing tables
+
 DROP TABLE IF EXISTS claims;
 DROP TABLE IF EXISTS policies;
 DROP TABLE IF EXISTS policyTypes;
 DROP TABLE IF EXISTS usersToContact;
 DROP TABLE IF EXISTS users;
+
+
+-- Create tables
 
 CREATE TABLE users (
     username NVARCHAR(100) PRIMARY KEY,
@@ -70,13 +75,21 @@ CREATE TABLE claims (
 	amount_approved FLOAT
 );
 
--- Add constraint to set default submission_date to current date/time when claim is created
--- ALTER TABLE claims
--- ADD CONSTRAINT DF_Claims_SubmissionDate DEFAULT (GETDATE()) FOR submission_date;
 
-INSERT INTO users (username, password, first_name, last_name, is_admin)
+-- Insert Dummy Users
+
+INSERT INTO users (username, password, first_name, last_name, is_admin, profile_pic)
 VALUES
-('admin', 'scrypt:32768:8:1$4Ce2JLFzCwuMqtsi$a804f8cda8d67c01dfabd8837936899f3bf80c433577b922dbb9f27997683d1b76d7698e992827118d5bea047495c5e249a39c1d894c266d6bfd133319c25eb1', 'Admin', 'Account', 1);
+('admin', 'scrypt:32768:8:1$4Ce2JLFzCwuMqtsi$a804f8cda8d67c01dfabd8837936899f3bf80c433577b922dbb9f27997683d1b76d7698e992827118d5bea047495c5e249a39c1d894c266d6bfd133319c25eb1', 'Admin', 'Account', 1, 'static/Image/user_uploads/admin-pfp.jpg');
+
+INSERT INTO users (username, password, first_name, last_name, is_admin, profile_pic, email, phone_number, id_number, claims_made, claims_pending)
+VALUES
+('john545',
+'scrypt:32768:8:1$4Ce2JLFzCwuMqtsi$a804f8cda8d67c01dfabd8837936899f3bf80c433577b922dbb9f27997683d1b76d7698e992827118d5bea047495c5e249a39c1d894c266d6bfd133319c25eb1',
+'John', 'Steven', 0, 'static/Image/user_uploads/policy-2025-04-09-john545.png', 'john@steve.com', '0713457593', '9908356789589', 1, 1);
+
+
+-- Insert Dummy Policy Types
 
 INSERT INTO policyTypes VALUES ('aFRESH', 'Fresher Plan', 'Covers Theft Protection', 50);
 INSERT INTO policyTypes VALUES ('bPERS', 'Personal Plan', 'Covers Theft Protection and Screen Damage Protection', 200);
@@ -84,18 +97,25 @@ INSERT INTO policyTypes VALUES ('cBUS', 'Business Plan', 'Covers Theft Protectio
 INSERT INTO policyTypes VALUES ('dPLAT', 'Platinum Plan', 'Covers Theft Protection, Screen Damage Protection, Water Damge Protection and Phone Upgrade Costs', 1000);
 
 
-INSERT INTO policies VALUES ('123', 100, 'iPhone 13', 'Business Plan', 'Yes', 'Yes', 'No',
-    (SELECT username FROM users WHERE username = 'admin'),
-    (SELECT policy_type_id FROM policyTypes WHERE policy_type_id='BPLAN'), 'test.jpg');
+-- Insert Dummy Policies
 
-INSERT INTO claims (claim_id, status, reason, admin_comment, date_of_incident, username, policy_id)
-VALUES ('claim1', 'Pending', 'broke my phone', '', '2025-03-15',
-    (SELECT username FROM users WHERE username = 'admin'),
-    (SELECT policy_id FROM policies WHERE policy_id='123'), 10000, 6000);
+INSERT INTO policies VALUES ('df781ac6-b6ca-40ff-921b-330e78d41ab2', 100, 'iPhone 13', 'Business Plan', 1, 1, 0,
+    (SELECT username FROM users WHERE username = 'john545'),
+    (SELECT policy_type_id FROM policyTypes WHERE policy_type_id='bPERS'), 'static/Image/user_uploads/policy-2025-04-09-john545.png');
+
+
+-- Insert Dummy Claims
+
+INSERT INTO claims (claim_id, status, reason, admin_comment, date_of_incident, username, policy_id, claim_amount, affidavit_link, quotation_link, image_link)
+VALUES ('ab1d9cc8-4531-46ac-9177-1053bac4b419', 'Pending', 'I dropped my phone.', '', '2025-04-01',
+    (SELECT username FROM users WHERE username = 'john545'),
+    (SELECT policy_id FROM policies WHERE policy_id='df781ac6-b6ca-40ff-921b-330e78d41ab2'), 10000, 'https://docs.google.com/document/d/1GiFhnbCby9gl3ZLHVGGRCxTy225CUEalv6RGjIzksjw/edit?usp=sharing', 'https://docs.google.com/document/d/1LqAU_adI2F6owms8HqOjvrVD4F4Qp-8WfuJ2qTd1SBY/edit?usp=sharing', 'https://www.gizchina.com/wp-content/uploads/images/2021/09/iPhone-13-Pro-a.jpg');
+
+
+-- View Data
 
 SELECT * FROM users;
 SELECT * FROM usersToContact;
 SELECT * FROM claims;
 SELECT * FROM policyTypes;
-
 SELECT * FROM policies;
